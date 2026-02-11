b5fc3d0f1655178b5842917872f1a4444aff5aa8
bennellis <brobertellis@gmail.com>
2026-02-11 11:37:32 +0100

    Update setup.sh to not use hardcoded CUDA_HOME, and to use no build isolation on some of the installs

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
-export CUDA_HOME="/usr/local/cuda-11.8"
+# export CUDA_HOME="/usr/local/cuda-11.8"
 pip install torch==2.0.1+cu118 torchvision xformers --extra-index-url https://download.pytorch.org/whl/cu118
 pip install diffusers[torch]==0.27.2
 pip install ipympl triton transformers 
 
 # Install local dependencies in editable mode
-pip install -e ./third_party/Mask2Former
-pip install -e ./third_party/ODISE
-pip install -e ./third_party/meshplot
-pip install -e ./third_party/stablediffusion
-pip install -e ./third_party/featup
-pip install -e ./third_party/dift
+pip install -e ./third_party/Mask2Former --no-build-isolation --config-settings editable_mode=compat
+pip install -e ./third_party/ODISE --no-build-isolation --config-settings editable_mode=compat
+pip install -e ./third_party/meshplot --no-build-isolation --config-settings editable_mode=compat
+pip install -e ./third_party/stablediffusion --no-build-isolation --config-settings editable_mode=compat
+pip install -e ./third_party/featup --no-build-isolation --config-settings editable_mode=compat
+pip install -e ./third_party/dift --no-build-isolation --config-settings editable_mode=compat
 pip install pythreejs torch-tb-profiler
 
 # diff3f dependencies
-CUDA_HOME=/usr/local/cuda-11.8 FORCE_CUDA=1 pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable"
+# CUDA_HOME=/usr/local/cuda-11.8 
+FORCE_CUDA=1 pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable" --no-build-isolation
 # DiffusionNet dependencies
 pip install trimesh rtree "pyglet<2" plyfile meshio robust_laplacian potpourri3d pywavefront
 
@@ -24,7 +25,7 @@ pip install igraph==0.11.5 # future verions dont allow integer as vertex names
 pip install pymeshlab==2023.12.post2
 pip install numpy==1.24.1 # needs to be <2
 pip install huggingface-hub==0.25.2
-pip install -e .
+pip install -e . --no-build-isolation --config-settings editable_mode=compat
 
 # install pre-commit hook
 cp pre-commit .git/hooks
